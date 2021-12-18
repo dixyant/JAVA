62cabc9ee5e8f1a0248b51dc1a94bd5ed533b4de
dixyant <58324939+dixyant@users.noreply.github.com>
2021-12-18 11:46:17 +0545

    abstract class

---
+++ Lab/Lab1/Q3/AbstractClass.java
@@ -0,0 +1,7 @@
+class AbstractClass{
+    public static void main(String [] args){
+        Cat kitty = new Cat();
+        System.out.println("Kitty is "+kitty.type+".");
+        kitty.sound();
+    }
+}
---
+++ Lab/Lab1/Q3/Cat.java
@@ -0,0 +1,7 @@
+class Cat extends Animal{
+    String type = "omnivore";
+    public void sound(){
+        System.out.println("It says moew.");
+    }
+
+}
